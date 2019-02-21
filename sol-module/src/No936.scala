3963873b2de303990799ff3e01346f8d00a339be
dylansun <lili.sun.zju@gmail.com>
2019-02-21 23:04:06 +0800

    update

--- sol-module/src/No936.scala
+++ sol-module/src/No936.scala
@@ -5,42 +5,33 @@ import com.sun.prism.impl.Disposer.Target
   */
 object No936 {
   def movesToStamp(stamp: String, target: String): Array[Int] = {
-    val fchar = 'A'
+    val fchar = 'X'; var s = target
     val f = target.map(x => fchar)
-    var s = target
     val rep = stamp.map(x => fchar)
-    val nmax = 10 * target.length; var n = 0
-    var ans = Array[Int]()
+    var n = 0; var ans = Array[Int]()
     val loopmax = s.length - stamp.length
     while(true){
       var found = false
       for(x <- 0 to loopmax){
         val sub = s.substring(x, x+stamp.length)
-        //println(s"n: $n, sub: $sub")
         if(isstamp(stamp, sub, fchar)){
           found = true
-          println(s"s: $s, sub: $sub, replacement: $rep")
-          //s = s.replaceFirst(sub, rep)// here
+          println(s"target: $s, sub: $sub, replacement: $rep")
           s = s.substring(0,x) + rep + s.substring(x+stamp.length)
           ans :+= x
-          println(s"replace s: $s, ans: ${ans.mkString}")
+          println(s"replaced target: $s, ans: ${ans.mkString}")
         }
       }
       n += 1
-      if(n == nmax || !found){
-        println(s"found: $found, s: $s")
-        if(s == f){
-          println(s"ans: ${ans.mkString}")
-          return ans.reverse
-        }
+      if(n == 10 * target.length || !found){
+        if(s == f) return ans.reverse
         else return Array[Int]()
       }
     }
     Array[Int]()
   }
   def isstamp(stamp: String, cdt: String, fchar: Char): Boolean = {
-    val qmark = stamp.map(x => fchar)
-    if(cdt == qmark) return false
+    if(cdt == stamp.map(x => fchar)) return false
     for(x <- stamp.indices if cdt(x) != stamp(x) && cdt(x) != fchar) return false
     true
   }
@@ -52,7 +43,7 @@ object No936 {
     println(na.substring(0,1))
 
 
-    println(movesToStamp("rpu", "rprrpurprpuurpu").mkString)
+    println(movesToStamp("abca", "aabcaca").mkString)
   }
 
 }
