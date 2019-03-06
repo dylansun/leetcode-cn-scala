57cc51479bc3af0d3c084b8edf1db55c7deb31f5
dylansun <lili.sun.zju@gmail.com>
2019-03-07 05:35:31 +0800

    update

--- sol-module/src/No971.scala
+++ sol-module/src/No971.scala
@@ -7,27 +7,26 @@ object No971 {
     val ans = solver(root, voyage)
     if(ans.contains(-1)) List(-1) else ans
   }
-  def solver(root: TreeNode, voyage: Array[Int]): List[Int] ={
-    root match {
-      case null => List[Int]()
-      case node: TreeNode if node.value != voyage.head => List(-1)
-      case node: TreeNode if node.left == null && node.right == null => List[Int]()
-      case node: TreeNode if node.left != null && node.right == null =>solver(node.left, voyage.tail)
-      case node: TreeNode if node.left == null && node.right != null =>solver(node.right, voyage.tail)
-      case node: TreeNode if node.left != null && node.right != null =>
-      {
-        val nl = countNode(root.left)
-        //val nr = countNode(root.right)
-        val ml = root.left.value == voyage.tail.head
-        val mr = root.right.value == voyage.tail.head
-        (ml, mr) match {
-          case (true, false) => solver(root.left, voyage.slice(1, nl+1)) ::: solver(root.right, voyage.slice(nl+1, voyage.length))
-          case (false, true) => root.value :: solver(root.right, voyage.slice(1, nl+1)) ::: solver(root.left, voyage.slice(nl+1, voyage.length))
-          case _ => List(-1)
-        }
+  def solver(root: TreeNode, voyage: Array[Int]): List[Int] = root match {
+    case null => List[Int]()
+    case node: TreeNode if node.value != voyage.head => List(-1)
+    case node: TreeNode if node.left == null && node.right == null => List[Int]()
+    case node: TreeNode if node.left != null && node.right == null =>solver(node.left, voyage.tail)
+    case node: TreeNode if node.left == null && node.right != null =>solver(node.right, voyage.tail)
+    case node: TreeNode if node.left != null && node.right != null =>
+    {
+      val nl = countNode(root.left)
+      val nr = countNode(root.right)
+      val ml = root.left.value == voyage.tail.head
+      val mr = root.right.value == voyage.tail.head
+      (ml, mr) match {
+        case (true, false) => solver(root.left, voyage.slice(1, nl+1)) ::: solver(root.right, voyage.slice(nl+1, voyage.length))
+        case (false, true) => root.value :: solver(root.right, voyage.slice(1, nr+1)) ::: solver(root.left, voyage.slice(nr+1, voyage.length))
+        case _ => List(-1)
       }
     }
   }
+
 
   def countNode(root: TreeNode): Int = root match {
     case null => 0
